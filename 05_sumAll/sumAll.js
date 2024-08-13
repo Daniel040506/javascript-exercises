35ce11e53a1890f26d51908b26abb7024295d50d
daniel040506 <danielk040506@gmail.com>
2024-08-12 18:43:32 -0700

    added solution 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let answer = 0;
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) return "ERROR";
+    if (num2 < num1) {
+        for (var i = num2; num2 <= num1; num2++) {
+            answer+= num2;
+        }
+        return answer
+    }
+    
+    for (var i = num1; num1 <= num2; num1++) {
+        answer+= num1;
+    }
+    return answer
 };
 
 // Do not edit below this line
